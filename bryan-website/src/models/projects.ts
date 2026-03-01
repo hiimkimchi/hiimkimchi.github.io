65bc718c82db08d3e16cb4c1b86e7ef6e46507d5
Bryan Kim <wabkim360@gmail.com>
2026-02-28 20:52:11 -0800

    CD error

--- bryan-website/src/models/projects.ts
+++ bryan-website/src/models/projects.ts
@@ -1,5 +1,7 @@
-export enum ProjectCardType {
-    REPO = "repo",
-    WEBSITE = "website",
-    NONE = "none",
-}
+export const ProjectCardType = {
+  REPO: "repo",
+  WEBSITE: "website",
+  NONE: "none",
+} as const;
+
+export type ProjectCardType = (typeof ProjectCardType)[keyof typeof ProjectCardType];
